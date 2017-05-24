8af25ee9afd604be5e38a8bd6e1a1f5fea896075
Sulaiman Ghori <anoneemousehax@gmail.com>
2017-05-24 14:40:13 -0400

    Updated drive code

--- InTheZone/ReRun/src/opcontrol.c
+++ InTheZone/ReRun/src/opcontrol.c
@@ -38,8 +38,7 @@ int J2C8BU[600];
 int J2C8BL[600];
 int J2C8BD[600];
 int J2C8BR[600];*/
-//Timer - update 1 time per driver cycle, every 25 msec
-int DriveTimer = 0;
+int DriveTimer = 0; //Timer - update 1 time per driver cycle, every 25 msec
 int RecSwitch = 1; // Switch between playback and record
 /*
  * Runs the user operator control code. This function will be started in its own task with the
@@ -58,12 +57,41 @@ int RecSwitch = 1; // Switch between playback and record
  *
  * This task should never exit; it should end with some kind of infinite loop, even if empty.
  */
+void setCache() { //Paste output here for cache
 
- void updateDrive() { //Update chasis to joystick control
-	 motorSet(1, joystickGetAnalog(1,2));
-	 motorSet(2, joystickGetAnalog(1,2));
-	 motorSet(3, joystickGetAnalog(1,3));
+}
+ void updateDrive() { //Update robot to joystick control
+   //Chasis control
+	 motorSet(2, -joystickGetAnalog(1,2));
+	 motorSet(3, -joystickGetAnalog(1,2));
 	 motorSet(4, joystickGetAnalog(1,3));
+	 motorSet(5, joystickGetAnalog(1,3));
+   //Lift control
+   if (joystickGetDigital(1, 6, JOY_UP) == 1){
+     motorSet(6, 127);
+     motorSet(7, 127);
+   }
+   if (joystickGetDigital(1, 6, JOY_DOWN) == 1){
+     motorSet(6, -127);
+     motorSet(7, -127);
+   }
+   if (joystickGetDigital(1, 6, JOY_DOWN) == 0 && joystickGetDigital(1, 6, JOY_UP) == 0){
+     motorSet(6, 0);
+     motorSet(7, 0);
+   }
+   //Claw control
+   if (joystickGetDigital(1, 5, JOY_UP) == 1){
+     motorSet(8, 127);
+     motorSet(9, 127);
+   }
+   if (joystickGetDigital(1, 5, JOY_DOWN) == 1){
+     motorSet(8, -127);
+     motorSet(9, -127);
+   }
+   if (joystickGetDigital(1, 5, JOY_DOWN) == 0 && joystickGetDigital(1, 5, JOY_UP) == 0){
+     motorSet(8, 0);
+     motorSet(9, 0);
+   }
  }
  void recordDrive() { // Record driver input while allowing driver control
 	 J1C2[DriveTimer] = joystickGetAnalog(1, 2);// Get analog value of vertical axis of right stick, joystick 1
@@ -73,35 +101,50 @@ int RecSwitch = 1; // Switch between playback and record
    J1C6BU[DriveTimer] = joystickGetDigital(1,6,JOY_UP); // Get boolean value of upper left bumper
 	 J1C6BD[DriveTimer] = joystickGetDigital(1,6,JOY_DOWN); // Get boolean value of lower left bumper*/
  }
+ void playbackDrive() { // Set motor to array values
+   motorSet(2, -J1C2[DriveTimer]);
+   motorSet(3, -J1C2[DriveTimer]);
+   motorSet(4, J1C3[DriveTimer]);
+   motorSet(5, J1C3[DriveTimer]);
+ }
+ void setMode() { // Check buttons to set mode
+   if (joystickGetDigital(1,7,JOY_DOWN) == 1){// If you push the lower button, turn on recording
+     RecSwitch = 1;
+     DriveTimer = 0;
+   }
+   if (joystickGetDigital(1,7,JOY_RIGHT) == 1){// If you push the right button, turn on normal drive
+     RecSwitch = 2;
+     DriveTimer = 0;
+   }
+   if (joystickGetDigital(1,7,JOY_LEFT) == 1){// If you push the left button, turn on playback from cache
+     RecSwitch = 3;
+     DriveTimer = 0;
+     setCache();
+   }
+   if (joystickGetDigital(1,7,JOY_UP)){ // If you push the upper button, turn off recording NOT USABLE
+     RecSwitch = 0;
+   }
+   if (RecSwitch == 1) { // Begin Record function
+     recordDrive();
+     updateDrive();
+   } // End Record function
+   if (RecSwitch == 2) { // Begin normal drive function
+     updateDrive();
+   }
+   if (RecSwitch == 3) { // Begin normal drive function
+     playbackDrive();
+   }
+   if (RecSwitch == 0) { // Begin playback function
+     playbackDrive();
+   }
+ }
 
 void operatorControl() { //Motor documentation, 1-2 left joystick, 3-4 right joystick
 	while (1) {
-    //J1C2[DriveTimer]; // Debug function, convert array of joystick 1 channel 2, input into stringp
-		if (joystickGetDigital(1,7,JOY_DOWN) == 1){// If you push the lower button, turn on recording
-			RecSwitch = 1;
-			DriveTimer = 0;
-		}
-		if (joystickGetDigital(1,7,JOY_RIGHT) == 1){// If you push the right button, turn on normal drive
-			RecSwitch = 2;
-			DriveTimer = 0;
-		}
-		if (joystickGetDigital(1,7,JOY_UP)){ // If you push the upper button, turn off recording
-			RecSwitch = 0;
-		}
-		if (RecSwitch == 1) { // Begin Record function
-			recordDrive();
-			updateDrive();
-		} // End Record function
-	  if (RecSwitch == 2) { // Begin normal drive function
-			updateDrive();
-		}
-		if (RecSwitch == 0) { // Begin playback function
-			motorSet(1, J1C2[DriveTimer]); // Set motor to array values
-			motorSet(2, J1C2[DriveTimer]);
-			motorSet(3, J1C3[DriveTimer]);
-			motorSet(4, J1C3[DriveTimer]);
-		}
-    printf("Value is %d, time is %d \n", J1C2[DriveTimer], DriveTimer);
+    setMode();
+    //printf("Value is %d, time is %d \n", J1C2[DriveTimer], DriveTimer);
+    printf("J1C2[%d] = %d \n", DriveTimer, J1C2[DriveTimer]); //Print statements formatted so that copy-paste will work
+    printf("J1C3[%d] = %d \n", DriveTimer, J1C3[DriveTimer]);
     DriveTimer = DriveTimer + 1; // Record time
 		delay(50); // Wait for refresh
 	}
