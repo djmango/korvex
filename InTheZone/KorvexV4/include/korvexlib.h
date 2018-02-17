e01352c0e4b4bfb39bcee7bcbe8414073b1e8c9d
Sulaiman Ghori <skghori03@gmail.com>
2018-02-16 21:52:52 -0500

    post day 1 of states commit

--- InTheZone/KorvexV4/include/korvexlib.h
+++ InTheZone/KorvexV4/include/korvexlib.h
@@ -10,9 +10,9 @@ Encoder dr4bencoder;
 int coneIncrementGlobal; // this will hold the amount of cones we currently have
 bool isDriverloadGlobal; // this will be true when we are stacking driver loads
 bool autoStackerEnabled; // this will be true when the auto stacker is doing its thing
+int chainBufferGlobal; // live tuning im loopy and i cant thinke weeeeeeeeeeeeeeeeeee
 
 // autonomous selector
-
 int lcdHoldGlobal; // this will hold the current screen, so we can have a 'scrollable' interface
 int auton;
 
@@ -30,9 +30,9 @@ void fineControlToggle(int fineBtn, int fineBtn2, int reverseBtn,
                        int reverseBtn2);
 void mobileGoalControl(int moboLiftBtnUp, int moboLiftBtnDown);
 void coneHandlerControl(int clawBtnUp, int clawBtnDown, int chainControl);
-void autoStackControl(int incrementUpBtn, int incrementDownBtn, 
+void autoStackControl(int incrementUpBtn, int incrementDownBtn,
                       int incrementResetBtn, int driverloadBtn,
-                      int fieldloadBtn, int incrementUpNoFuncBtn);
+                      int fieldloadBtn, int incrementUpNoFuncBtn, int resetBtn, int bufferUp, int bufferDown);
 
 // drive control
 void driveTo(int leftTarget, int rightTarget, int waitTo); // drive pd control tuned for normal match
